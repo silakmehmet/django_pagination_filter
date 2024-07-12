267e25ef9267c5f85158430541882177fd844b90
silakmehmet <silakmehmet@gmail.com>
2024-07-12 13:20:06 +0300

    Ordering added locally

--- student_path/views.py
+++ student_path/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Path, Student
@@ -21,6 +21,7 @@ class StudentMVS(ModelViewSet):
     serializer_class = StudentSerializer
     pagination_class = CustomPageNumberPagination
     # pagination_class = CustomCursorPagination
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["first_name", "number"]
+    ordering_fields = ["number"]
     search_fields = ["first_name", "last_name"]
