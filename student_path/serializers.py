0f7b5feb6d5ff69017c251c83d5da0acc5c473ef
silakmehmet <silakmehmet@gmail.com>
2024-07-12 11:03:51 +0300

    PageNumberPagination added globally

--- student_path/serializers.py
+++ student_path/serializers.py
@@ -7,11 +7,11 @@ class PathSerializer(serializers.ModelSerializer):
     class Meta:
         model = Path
         fields = "__all__"
-        read_only_fields = ("id")
+        read_only_fields = ("id",)
 
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = "__all__"
-        read_only_fields = ("id")
+        read_only_fields = ("id",)
